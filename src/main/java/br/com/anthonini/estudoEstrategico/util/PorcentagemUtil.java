92e915204069e406aea03d7733304ca0f8585307
Anthonini <anthonnini@gmail.com>
2021-12-05 17:58:28 -0300

    Corrigido cálculo de porcentagem de questões.
    - Quando a quantidade de questões certas era zero (a pessoa não acertou
    nenhuma questão) a porcentagem não aparecia como 0,00%
    
    
    Signed-off-by: Anthonini <anthonnini@gmail.com>

--- src/main/java/br/com/anthonini/estudoEstrategico/util/PorcentagemUtil.java
+++ src/main/java/br/com/anthonini/estudoEstrategico/util/PorcentagemUtil.java
@@ -5,8 +5,8 @@ import java.text.DecimalFormat;
 public class PorcentagemUtil {
 
 	public static String getPorcentagem(Integer quantidade, Integer total) {
-		if(quantidade != null && quantidade > 0 && total != null && total > 0) {
-			DecimalFormat numberFormat = new DecimalFormat("#.00");
+		if(quantidade != null && quantidade >= 0 && total != null && total > 0) {
+			DecimalFormat numberFormat = new DecimalFormat("#0.00");
 			return numberFormat.format( ((double)quantidade/total)*100 ) + "%";
 		}
 		
