80b7de3a9a0fe74a46864d457d57ccd4e26bb888
Anthonini <anthonnini@gmail.com>
2021-12-05 17:45:09 -0300

    Correção dos parâmetros de configuração de e-mail.
    Signed-off-by: Anthonini <anthonnini@gmail.com>

--- src/main/java/br/com/anthonini/estudoEstrategico/config/MailConfig.java
+++ src/main/java/br/com/anthonini/estudoEstrategico/config/MailConfig.java
@@ -13,19 +13,19 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @PropertySource(value = { "file:./.estudo_estrategico-mail.properties" }, ignoreResourceNotFound = true)
 public class MailConfig {
 	
-	@Value("${external.estudo_estrategico.mail.host:${estudo_estrategico.mail.host}}")
+	@Value("${estudo_estrategico.mail.host}")
 	private String host;
 	
-	@Value("${external.estudo_estrategico.mail.port:${estudo_estrategico.mail.port}}")
+	@Value("${estudo_estrategico.mail.port}")
 	private int port;
 	
-	@Value("${external.estudo_estrategico.mail.username:${estudo_estrategico.mail.username}}")
+	@Value("${estudo_estrategico.mail.username}")
 	private String username;
 	
-	@Value("${external.estudo_estrategico.mail.password:${estudo_estrategico.mail.password}}")
+	@Value("${estudo_estrategico.mail.password}")
 	private String password;
 	
-	@Value("${external.estudo_estrategico.mail.from:${estudo_estrategico.mail.from}}")
+	@Value("${estudo_estrategico.mail.from}")
 	private String fromEmail;
 
 	@Bean
