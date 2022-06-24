be6ba77233c22acd66621c8b724fa58a62c5c64e
Anthonini <anthonini@info.ufrn.br>
2022-06-24 06:04:56 -0300

    Criado migração com as permissões:
    - Cadastrar Permissões
    - Cadastrar Grupo de Usuário
    - Professor
    
    Signed-off-by: Anthonini <anthonini@info.ufrn.br>

---
+++ src/main/resources/db/migration/V01__criar_permissoes.sql
@@ -0,0 +1,3 @@
+INSERT INTO permissao (id_permissao, nome, descricao) VALUES (1, 'ROLE_CADASTRAR_PERMISSOES', 'Cadastrar Permissões');
+INSERT INTO permissao (id_permissao, nome, descricao) VALUES (2, 'ROLE_CADASTRAR_GRUPO_USUARIO', 'Cadastrar Grupo de Usuário');
+INSERT INTO permissao (id_permissao, nome, descricao) VALUES (3, 'ROLE_PROFESSOR', 'Professor');
