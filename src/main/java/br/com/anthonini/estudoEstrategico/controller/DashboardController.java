bf5a23efc500c94501fe5a161b1f03fc5e3076c3
Anthonini <anthonini@info.ufrn.br>
2022-07-02 01:57:00 -0300

    Desativando a página de dashboard.
    Signed-off-by: Anthonini <anthonini@info.ufrn.br>

--- src/main/java/br/com/anthonini/estudoEstrategico/controller/DashboardController.java
+++ src/main/java/br/com/anthonini/estudoEstrategico/controller/DashboardController.java
@@ -1,12 +1,8 @@
 package br.com.anthonini.estudoEstrategico.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-@RequestMapping("/dashboard")
 public class DashboardController {
 	
 	@GetMapping
