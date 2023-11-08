d3e1fb609410a87804834430564b9dbc6372216e
Anthonini <anthonini@info.ufrn.br>
2023-11-08 19:51:40 -0300

    Atualizado a migração de permissões.
    Signed-off-by: Anthonini <anthonini@info.ufrn.br>

--- src/main/resources/db/migration/V1_0_0_0000__criar_permissoes.sql
+++ src/main/resources/db/migration/V1_0_0_0000__criar_permissoes.sql
@@ -1,3 +1,4 @@
 INSERT INTO permissao (id_permissao, nome, descricao) VALUES (1, 'ROLE_CADASTRAR_PERMISSOES', 'Cadastrar Permissões');
 INSERT INTO permissao (id_permissao, nome, descricao) VALUES (2, 'ROLE_CADASTRAR_GRUPO_USUARIO', 'Cadastrar Grupo de Usuário');
 INSERT INTO permissao (id_permissao, nome, descricao) VALUES (3, 'ROLE_PROFESSOR', 'Professor');
+INSERT INTO permissao (id_permissao, nome, descricao) VALUES (4, 'ROLE_REALIZAR_MIGRACOES', 'Professor');
