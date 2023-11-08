d7369fcb48ab2bb9e1e8d674bfb39e68dfdce61d
Anthonini <anthonini@info.ufrn.br>
2023-11-08 13:50:58 -0300

    Criado a configuração para o flyway.
    Signed-off-by: Anthonini <anthonini@info.ufrn.br>

---
+++ src/main/java/br/com/anthonini/estudoEstrategico/config/FlywayConfig.java
@@ -0,0 +1,33 @@
+package br.com.anthonini.estudoEstrategico.config;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public Flyway flyway() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .outOfOrder(false)
+                .baselineOnMigrate(true)
+                .baselineVersion("0")
+                .sqlMigrationPrefix("V")
+                .sqlMigrationSeparator("__")
+                .sqlMigrationSuffixes(".sql")
+                .locations("classpath:db/migration")
+                .schemas("migration")
+                .table("schema_version_conversor_migration")
+                .load();
+        
+        return flyway;
+    }
+}	
