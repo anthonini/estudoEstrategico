c8b64ce074e10832c9a5e2c52e6efd39bd9bf18d
Anthonini <anthonini@info.ufrn.br>
2023-11-10 11:48:20 -0300

    Criado a permissão de gerenciar usuários.
    Signed-off-by: Anthonini <anthonini@info.ufrn.br>

--- src/main/resources/db/migration/V1_0_0_0000__criar_permissoes.sql
+++ src/main/resources/db/migration/V1_0_0_0000__criar_permissoes.sql
@@ -1,4 +1,5 @@
 INSERT INTO permissao (id_permissao, nome, descricao) VALUES (1, 'ROLE_CADASTRAR_PERMISSOES', 'Cadastrar Permissões');
 INSERT INTO permissao (id_permissao, nome, descricao) VALUES (2, 'ROLE_CADASTRAR_GRUPO_USUARIO', 'Cadastrar Grupo de Usuário');
-INSERT INTO permissao (id_permissao, nome, descricao) VALUES (3, 'ROLE_PROFESSOR', 'Professor');
-INSERT INTO permissao (id_permissao, nome, descricao) VALUES (4, 'ROLE_IMPLANTAR_MIGRACOES', 'Implantar Migrações');
+INSERT INTO permissao (id_permissao, nome, descricao) VALUES (3, 'ROLE_GERENCIAR_USUARIOS', 'Gerenciar Usuários');
+INSERT INTO permissao (id_permissao, nome, descricao) VALUES (4, 'ROLE_PROFESSOR', 'Professor');
+INSERT INTO permissao (id_permissao, nome, descricao) VALUES (5, 'ROLE_IMPLANTAR_MIGRACOES', 'Implantar Migrações');
